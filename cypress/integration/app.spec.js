875c6c60d735e584195e9864ed4bef4deceda361
Juan Antonio Fernández de Alba <juan@undefinedlabs.com>
2020-02-03 17:41:12 +0100

    fix e2e test

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,7 +1,7 @@
 describe('app', () => {
   it('can visit the app', () => {
     cy.visit('/')
-      .getByText(/submit a new restaurant/i)
+      .findByText(/submit a new restaurant/i)
       .should('exist')
   })
 })
