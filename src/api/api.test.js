51f2c673e6da49026e6af7bc2cdcb09d36640732
Juan Antonio Fernández de Alba <juan@undefinedlabs.com>
2020-02-11 12:09:42 +0100

    add integration test

--- src/api/api.test.js
+++ src/api/api.test.js
@@ -2,4 +2,8 @@ describe('api', () => {
   it('can fetch all restaurants', async () => {
     expect(true).toEqual(true)
   })
+  it('integration test', async () => {
+    const response = await fetch('https://go-demo-app.undefinedlabs.dev/restaurants/')
+    expect(response.status).toEqual(200)
+  })
 })
