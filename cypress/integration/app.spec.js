94d3c44b67b9ef34f07a53a0393bab777539b88d
Juan Antonio Fernández de Alba <juan@undefinedlabs.com>
2020-03-31 18:52:06 +0200

    Change Name of Integration Tests (#59)
    Closes #51
    
    Changes:
    
    - Change name of integration tests.
    - Run them twice.

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,12 +1,33 @@
 const fileName = 'scope.png'
 
-describe('integration tests', () => {
+describe('demotest', () => {
   const restaurantToAdd = `newRestaurant${1 + Math.floor(Math.random() * 1e6)}`
 
   it('can visit the app', () => {
     cy.visit('/')
       .findByText(/submit a new restaurant/i)
       .should('exist')
+  })
+  it('can visit the app', () => {
+    cy.visit('/')
+      .findByText(/submit a new restaurant/i)
+      .should('exist')
+  })
+  it('can submit a new restaurant', () => {
+    cy.visit('/')
+      .get('#submit-name')
+      .type(restaurantToAdd)
+      .get('#submit-description')
+      .type('description that is good')
+      .fixture(fileName)
+      .then(fileContent => {
+        cy.get('#imageUpload')
+          .upload({ fileContent, fileName, mimeType: 'image/png' })
+          .get('[type=submit]')
+          .click()
+          .findByText(restaurantToAdd)
+          .should('exist')
+      })
   })
   it('can submit a new restaurant', () => {
     cy.visit('/')
@@ -55,6 +76,17 @@ describe('integration tests', () => {
       .get(`.current-rate-${restaurantToAdd}`)
       .should('have.length', 3)
   })
+  it('can rate a restaurant', () => {
+    cy.visit('/')
+      .get('#search')
+      .type(restaurantToAdd)
+      .get(`#rate-${restaurantToAdd}`)
+      .click()
+      .get(`#rate-star-3`)
+      .click()
+      .get(`.current-rate-${restaurantToAdd}`)
+      .should('have.length', 3)
+  })
   it('can delete a restaurant', () => {
     cy.visit('/')
       .get('#search')
@@ -64,7 +96,35 @@ describe('integration tests', () => {
       .findByText(restaurantToAdd)
       .should('not.exist')
   })
+  it('can delete a restaurant', () => {
+    cy.visit('/')
+      .get('#search')
+      .type(restaurantToAdd)
+      .get(`#delete-2-${restaurantToAdd}`)
+      .click()
+      .findByText(restaurantToAdd)
+      .should('not.exist')
+  })
 
+  it('can submit and delete a restaurant with an empty image', () => {
+    const emptyImageRestaurant = `empty${1 + Math.floor(Math.random() * 1e6)}`
+    cy.visit('/')
+      .get('#submit-name')
+      .type(emptyImageRestaurant)
+      .get('#submit-description')
+      .type('description that is good')
+      .get('[type=submit]')
+      .click()
+      .then(() => {
+        cy.findByText(emptyImageRestaurant)
+          .should('exist')
+          .get(`#delete-${emptyImageRestaurant}`)
+          .click()
+          .wait(1000)
+          .findByText(emptyImageRestaurant)
+          .should('not.exist')
+      })
+  })
   it('can submit and delete a restaurant with an empty image', () => {
     const emptyImageRestaurant = `empty${1 + Math.floor(Math.random() * 1e6)}`
     cy.visit('/')
